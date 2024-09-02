f8f2485669d4894200df50c172682d7abab2c427
VasylPopaz <pafos1997@gmail.com>
2024-09-02 18:45:54 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl text-slate-200">Book Manager</h1>
+    </div>
+  );
+};
