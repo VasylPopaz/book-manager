16c5e11f6a84ed580c2f4583c0372f3669aac8e2
VasylPopaz <pafos1997@gmail.com>
2024-09-02 20:26:43 +0200

    add container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "sm-max": { max: "374.99px" },
+      md: "768px",
+      lg: "1440px",
+    },
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "18px",
+          paddingRight: "18px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            paddingLeft: "128px",
+            paddingRight: "128px",
+            maxWidth: "1440px",
+          },
+        },
+      });
+    },
+  ],
 };
