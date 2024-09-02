42a89e36cdf8021515f66ac45544fb8fe7a8ec23
VasylPopaz <pafos1997@gmail.com>
2024-09-02 23:27:17 +0200

    add sort books

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,6 +8,11 @@ import type { Book } from "../types";
 export const App = () => {
   const [books, setBooks] = useState<Book[] | null>(null);
 
+  const [sortConfig, setSortConfig] = useState<{
+    sortField: keyof Book | null;
+    direction: "asc" | "desc";
+  }>({ sortField: null, direction: "asc" });
+
   useEffect(() => {
     getBooks().then((res) => {
       setBooks(res.books);
@@ -16,9 +21,40 @@ export const App = () => {
 
   if (!books) return;
 
+  const handleSortClick = (sortField: keyof Book) => {
+    let direction: "asc" | "desc" = "asc";
+
+    if (sortConfig.sortField === sortField && sortConfig.direction === "asc") {
+      direction = "desc";
+    }
+
+    const sortedBooks = [...books].sort((a, b) => {
+      if (
+        typeof a[sortField] === "string" &&
+        typeof b[sortField] === "string"
+      ) {
+        return direction === "asc"
+          ? (a[sortField] as string).localeCompare(b[sortField] as string)
+          : (b[sortField] as string).localeCompare(a[sortField] as string);
+      }
+      if (
+        typeof a[sortField] === "boolean" &&
+        typeof b[sortField] === "boolean"
+      ) {
+        return direction === "asc"
+          ? (a[sortField] ? 1 : -1) - (b[sortField] ? 1 : -1)
+          : (b[sortField] ? 1 : -1) - (a[sortField] ? 1 : -1);
+      }
+      return 0;
+    });
+
+    setBooks(sortedBooks);
+    setSortConfig({ sortField, direction });
+  };
+
   return (
     <div className="container py-8">
-      <BookList books={books} />
+      <BookList books={books} handleSortClick={handleSortClick} />
     </div>
   );
 };
--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -3,9 +3,10 @@ import type { Book } from "../../types";
 
 interface BookListProps {
   books: Book[];
+  handleSortClick: (sortField: keyof Book) => void;
 }
 
-export const BookList = ({ books }: BookListProps) => {
+export const BookList = ({ books, handleSortClick }: BookListProps) => {
   const thStyles =
     "font-bold bg-[#7f26ba] py-[10px] px-[15px] cursor-pointer transition duration-300 first:rounded-tl-lg last:rounded-tr-lg";
   return (
@@ -13,11 +14,24 @@ export const BookList = ({ books }: BookListProps) => {
       <table className="w-full text-left">
         <thead className="sticky top-0 text-slate-50">
           <tr>
-            <th className={thStyles}>№</th>
-            <th className={thStyles}>Title</th>
-            <th className={thStyles}>Author</th>
-            <th className={thStyles}>ISBN</th>
-            <th className={thStyles}>Status</th>
+            <th className={thStyles} onClick={() => handleSortClick("_id")}>
+              №
+            </th>
+            <th className={thStyles} onClick={() => handleSortClick("title")}>
+              Title
+            </th>
+            <th className={thStyles} onClick={() => handleSortClick("author")}>
+              Author
+            </th>
+            <th className={thStyles} onClick={() => handleSortClick("isbn")}>
+              ISBN
+            </th>
+            <th
+              className={thStyles}
+              onClick={() => handleSortClick("isBorrowed")}
+            >
+              Status
+            </th>
             <th className={thStyles}></th>
           </tr>
         </thead>
