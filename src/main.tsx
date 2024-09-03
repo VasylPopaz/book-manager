af5fd55675660f54c2b73d36641f422e8f4d00ca
VasylPopaz <pafos1997@gmail.com>
2024-09-03 12:45:16 +0200

    upd table styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { App } from "./components";
@@ -9,6 +9,10 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <App />
-    <ToastContainer autoClose={3000} />
+    <ToastContainer
+      autoClose={2500}
+      transition={Zoom}
+      style={{ zIndex: 999 }}
+    />
   </>
 );
